24ca90f9050eabc270b718333e6d8ad44e902e11
EGOIST <0x142857@gmail.com>
2017-01-10 12:42:19 +0800

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# anyDoc
+
+In case you didn't know, you can use [docute](https://github.com/egoist/docute) to serve any file from anywhere, the **anyDoc** project is showing a use case that using docute to serve README.md for GitHub repo.
+
+For example, the [airbnb javascript styleguide](https://github.com/airbnb/javascript) is quite long, right? 
+
+You can visit https://egoistian.com/anydoc/#/?repo=airbnb/javascript instead to have a better reading experience.
+
+Cheers. 🍻
---
+++ docs/config.js
@@ -0,0 +1,32 @@
+;(function (m) {
+  function isUrl(input) {
+    return /$https?:\/\//.test(input)
+  }
+
+  function isRepo(input) {
+    return /^[\w\-]+\/[\w\-]+$/.test(input)
+  }
+
+  function anyDoc(ctx) {
+    ctx.router.beforeEach(function (to, from, next) {
+      var repo = to.query.repo
+      var url = isUrl(repo) ? 
+        repo :
+        'https://raw.githubusercontent.com/' + repo + '/master/README.md'
+
+      ctx.store.state.config.home = url
+      if (isRepo(repo)) {
+        ctx.store.state.config.title = repo
+        ctx.store.state.config.repo = repo
+      }
+      next()
+    })
+  }
+
+  m.$config = {
+    debug: true,
+    plugins: [
+      anyDoc
+    ]
+  }
+})(self);
